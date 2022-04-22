fada5dfb42081691e2e77898be75556555dc7e20
Nathan Holt <nateofakin@gmail.com>
2022-04-22 11:05:00 -0400

    added a bunch of extras

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,21 +9,49 @@ public class Main {
 
         double price = 12.5;
         int age;
+        boolean hasToy = false;
+        boolean hasSticker = false;
+        boolean hasDrink = false;
+        boolean hasDiaper = false;
 
 
         System.out.println("Please enter your age");
         age = scan.nextInt();
         scan.nextLine();
-
+//        \n \s \t
 
         if (age <= 10) {
             price = 5;
+
+            if (age <= 1) {
+                hasDiaper = true;
+            }else if (age < 5) {
+                hasToy = true;
+            }
         } else if (age > 65) {
             price = 8;
+
+            if (age > 85) {
+                hasDrink = true;
+            } else if (age > 75) {
+                hasSticker = true;
+            }
         }
 
 //        print out result
         System.out.println("Your ticket price is: " + numFor.format(price));
+
+        if (hasToy) {
+            System.out.println("You get a toy!");
+        } else if (hasDiaper) {
+            System.out.println("You get a diaper!");
+        } else if (hasSticker) {
+            System.out.println("You get a sticker!");
+        } else if (hasDrink) {
+            System.out.println("You get a free drink!");
+        } else {
+            System.out.println("You get no extra");
+        }
     }
 }
 
