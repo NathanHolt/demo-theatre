e4c31d07684a92d43d03d21a59e123f8b1050942
Nathan Holt <nateofakin@gmail.com>
2022-04-22 11:27:24 -0400

    Refactored extras

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,11 @@ public class Main {
         NumberFormat numFor = NumberFormat.getCurrencyInstance();
 
         double price = 12.5;
-        int age;
-        boolean hasToy = false;
-        boolean hasSticker = false;
-        boolean hasDrink = false;
-        boolean hasDiaper = false;
+        int age = 0;
+        String extra = "";
+        int count;
+
+//        String test = extra + " yay";
 
 
         System.out.println("Please enter your age");
@@ -24,30 +24,30 @@ public class Main {
             price = 5;
 
             if (age <= 1) {
-                hasDiaper = true;
+                extra = "diaper";
             }else if (age < 5) {
-                hasToy = true;
+                extra = "toy";
             }
         } else if (age > 65) {
             price = 8;
 
             if (age > 85) {
-                hasDrink = true;
+                extra = "drink";
             } else if (age > 75) {
-                hasSticker = true;
+                extra = "sticker";
             }
         }
 
 //        print out result
         System.out.println("Your ticket price is: " + numFor.format(price));
 
-        if (hasToy) {
+        if (extra.equals("toy")) {
             System.out.println("You get a toy!");
-        } else if (hasDiaper) {
+        } else if (extra.equals("diaper")) {
             System.out.println("You get a diaper!");
-        } else if (hasSticker) {
+        } else if (extra.equals("sticker")) {
             System.out.println("You get a sticker!");
-        } else if (hasDrink) {
+        } else if (extra.equals("drink")) {
             System.out.println("You get a free drink!");
         } else {
             System.out.println("You get no extra");
